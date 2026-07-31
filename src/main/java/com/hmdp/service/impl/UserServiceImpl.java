18581a65d14bc156319cf0032a71bed1c6d5cba7
jianghaojia <2908487619@qq.com>
2026-07-31 16:31:02 +0800

    1.创建login接口 2.通过redis存储用户token信息

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,22 +1,30 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.bean.copier.CopyOptions;
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.RegexUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 import static com.hmdp.utils.RedisConstants.*;
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 
 /**
@@ -50,6 +58,48 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
 
     @Override
     public Result login(LoginFormDTO loginForm, HttpSession session) {
-        return null;
+        // 1.校验手机号
+        if (RegexUtils.isPhoneInvalid(loginForm.getPhone())){
+            return  Result.fail("手机号错误");
+        }
+    //     校验验证码
+        String random=stringRedisTemplate.opsForValue().get(LOGIN_USER_KEY+loginForm.getPhone());
+        // 如果验证码不正确
+        if (random==null||!random.equals(loginForm.getCode())){
+            return  Result.fail("验证码不正确");
+        }
+        //查询用户是否存在
+        User user=query().eq("phone",loginForm.getPhone()).one();
+        log.info("用户详细信息:{}",user);
+        if (user==null){
+            //不存在就创建user
+             user=CreateUserWithPhone(loginForm.getPhone());
+        }
+        String token= UUID.randomUUID().toString();
+        //将user转成map对象存储
+        UserDTO userDTO= BeanUtil.copyProperties(user,
+                UserDTO.class);
+        Map<String,Object> map=BeanUtil.beanToMap(userDTO,new HashMap<>(),
+                CopyOptions.create()
+                        .setIgnoreNullValue(true).
+                        setFieldValueEditor((fieldName, fieldValue) ->
+                                fieldValue.toString())
+        );
+        //通过token存储user信息
+        String tokenKey=LOGIN_USER_KEY+token;
+        stringRedisTemplate.opsForHash().putAll(tokenKey,map);
+        return Result.ok(token);
+    }
+
+    /**
+     * 创建user用户
+     * @param phone
+     * @return
+     */
+    private User CreateUserWithPhone(String phone) {
+        User user=User.builder().phone(phone).nickName(USER_NICK_NAME_PREFIX+RandomUtil.randomString(10)).build();
+        //插入user
+        save(user);
+        return user;
     }
 }
