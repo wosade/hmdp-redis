a526e967351502d4a358a430ed91f42eb0959b11
jianghaojia <2908487619@qq.com>
2026-08-02 13:27:33 +0800

    让id为 时间戳+id

--- src/main/java/com/hmdp/controller/VoucherOrderController.java
+++ src/main/java/com/hmdp/controller/VoucherOrderController.java
@@ -2,6 +2,7 @@ package com.hmdp.controller;
 
 
 import com.hmdp.dto.Result;
+import com.hmdp.service.IVoucherOrderService;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +19,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/voucher-order")
 public class VoucherOrderController {
+    private IVoucherOrderService voucherOrderService;
     @PostMapping("seckill/{id}")
     public Result seckillVoucher(@PathVariable("id") Long voucherId) {
-        return Result.fail("功能未完成");
+        return voucherOrderService.seckillVouncher(voucherId);
     }
 }
